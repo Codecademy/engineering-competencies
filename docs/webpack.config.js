23776ec7fe02193a94469cb1802399cc93dd6861
Kip Price <4605379+kipprice@users.noreply.github.com>
2020-06-30 22:09:49 -0400

    initial TS setup

---
+++ docs/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'compMatrix.js',
+        path: path.resolve(__dirname, 'compiled_js'),
+        
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                sourceMap: true,
+                terserOptions: {
+                    keep_fnames: true,
+                    keep_classnames: true
+                }
+            })
+        ]
+    }
+};	
