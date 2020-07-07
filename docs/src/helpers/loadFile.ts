5aa7c33f1f1c50730c7a5594403c93c918074d8c
Kip Price <4605379+kipprice@users.noreply.github.com>
2020-07-07 10:57:47 -0400

    add managers

--- docs/src/helpers/loadFile.ts
+++ docs/src/helpers/loadFile.ts
@@ -1,12 +1,11 @@
-import { loadFile as load } from '@toolkip/server';
+import { loadFile as load } from "@toolkip/server";
 
 export const loadEngCompetencies = async () => {
-    const fileContents = await load({ filename: '../README.md' });
-    return fileContents;
-}
+  const fileContents = await load({ filename: "./README.md" });
+  return fileContents;
+};
 
 export const loadManagerCompetencies = async () => {
-    const fileContents = await load({ filename: '../managers.md' });
-    return fileContents;
-}
-
+  const fileContents = await load({ filename: "./managers.md" });
+  return fileContents;
+};
--- docs/src/index.ts
+++ docs/src/index.ts
@@ -1,20 +1,21 @@
-import { loadEngCompetencies, loadManagerCompetencies } from './helpers/loadFile';
-import { parseFile } from './helpers/parseFile';
-import { renderList } from './views/list';
-import { updateState } from './models/state';
-import { renderLayout } from './views/layout';
+import {
+  loadEngCompetencies,
+  loadManagerCompetencies,
+} from "./helpers/loadFile";
+import { parseFile } from "./helpers/parseFile";
+import { renderList } from "./views/list";
+import { updateState } from "./models/state";
+import { renderLayout } from "./views/layout";
 
 const _init = async () => {
-    // render competencies
-    renderLayout();
+  // render competencies
+  renderLayout();
 
-    // load competencies
-    const ic = await loadEngCompetencies();
-    //const em = await loadManagerCompetencies();
-    const parsed = parseFile(ic);
-    updateState(parsed);
+  // load competencies
+  const ic = await loadEngCompetencies();
+  const em = await loadManagerCompetencies();
+  const parsed = parseFile(ic + em);
+  updateState(parsed);
+};
 
-    
-}
-
-window.addEventListener('load', _init);
+window.addEventListener("load", _init);
