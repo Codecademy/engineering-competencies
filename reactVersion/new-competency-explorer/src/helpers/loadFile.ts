75e81184115a3e7e0be9c3eabd091411d23e2ad9
Kip Price <4605379+kipprice@users.noreply.github.com>
2020-07-11 09:41:33 -0400

    commit everything

--- reactVersion/new-competency-explorer/src/helpers/loadFile.ts
+++ reactVersion/new-competency-explorer/src/helpers/loadFile.ts
@@ -1,29 +1,14 @@
-import ic from '../../../../README.md';
-import em from '../../../../managers.md';
-
-const loadFile = async ({ filename }: { filename: string }) => {
-  // return new Promise<string>((resolve) => {
-  //   const rawFile = new XMLHttpRequest();
-  //   rawFile.open("GET", filename, false);
-  //   rawFile.onreadystatechange = () => {
-  //     if (rawFile.readyState === 4) {
-  //       if (rawFile.status === 200 || rawFile.status === 0) {
-  //         resolve(rawFile.responseText);
-  //       }
-  //     }
-  //   };
-  //   rawFile.send(null);
-  // });
-  const response = await fetch(ic);
+const loadFile = async (filename: string) => {
+  const response = await fetch(filename);
   return response.text();
 };
 
 export const loadEngCompetencies = async () => {
-  const fileContents = await loadFile({ filename: "../../../README.md" });
+  const fileContents = await loadFile("README.md");
   return fileContents;
 };
 
 export const loadManagerCompetencies = async () => {
-  const fileContents = await loadFile({ filename: "../../../managers.md" });
+  const fileContents = await loadFile("managers.md");
   return fileContents;
 };
--- reactVersion/new-competency-explorer/webpack.config.js
+++ reactVersion/new-competency-explorer/webpack.config.js
@@ -1,51 +1,53 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   // the output bundle won't be optimized for production but suitable for development
-  mode: 'development',
+  mode: "development",
   // the app entry point is /src/index.js
-  entry: path.resolve(__dirname, 'src', 'index'),
+  entry: path.resolve(__dirname, "src", "index"),
   output: {
-  	// the output of the webpack build will be in /dist directory
-    path: path.resolve(__dirname, 'dist'),
+    // the output of the webpack build will be in /dist directory
+    path: path.resolve(__dirname, "dist"),
     // the filename of the JS bundle will be bundle.js
-    filename: 'bundle.js'
+    filename: "bundle.js",
   },
   module: {
     rules: [
-       // we use babel-loader to load our jsx and tsx files
-       {
+      // we use babel-loader to load our jsx and tsx files
+      {
         test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: "babel-loader",
         },
       },
 
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"],
       },
 
       {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
-          'style-loader',
+          "style-loader",
           // Translates CSS into CommonJS
-          'css-loader',
+          "css-loader",
           // Compiles Sass to CSS
-          'sass-loader',
+          "sass-loader",
         ],
       },
-    ]
+    ],
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
   // add a custom index.html as the template
   plugins: [
-      new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') })
-    ]
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+  ],
 };
