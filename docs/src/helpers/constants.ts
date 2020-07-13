c6e248f03e43f5718b23df80f66c4df1787185c5
Kip Price <4605379+kipprice@users.noreply.github.com>
2020-07-12 20:54:42 -0400

    retry file paths

--- docs/src/helpers/constants.ts
+++ docs/src/helpers/constants.ts
@@ -1,5 +1,5 @@
 export const EXPAND_COLLAPSE_ICON = './res/down_caret.png';
 export const EX_ICON = './res/ex.png';
 
-export const IC_COMPETENCIES = './README.md';
-export const MANAGER_COMPETENCIES = './managers.md'
+export const IC_COMPETENCIES = '../README.md';
+export const MANAGER_COMPETENCIES = '../managers.md'
