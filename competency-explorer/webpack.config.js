bbbf37e99fb4ef5b2554a17ca3186fe0cef3055f
Kip Price <4605379+kipprice@users.noreply.github.com>
2020-07-12 21:40:38 -0400

    update index

--- competency-explorer/webpack.config.js
+++ competency-explorer/webpack.config.js
@@ -53,7 +53,7 @@ module.exports = {
 
   devServer: {
     contentBase: [path.join(__dirname, '../'), path.join(__dirname, '../..')],
-    publicPath: '/dist/',
+    publicPath: '/competency-explorer/dist/',
     compress: true,
     port: 5000
   }
